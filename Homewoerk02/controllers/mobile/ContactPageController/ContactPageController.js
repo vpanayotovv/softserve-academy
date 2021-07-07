7dfe458e36f11f24c86ecd962edfa9f75d5b3ca6
vpanayotovv <vvpanayotovv@gmail.com>
2021-07-07 03:22:32 +0300

    implemented contact lis with functionality

--- Homewoerk02/controllers/mobile/ContactPageController/ContactPageController.js
+++ Homewoerk02/controllers/mobile/ContactPageController/ContactPageController.js
@@ -1,5 +1,26 @@
 define({ 
 
- //Type your controller code here 
+  onViewCreated: function() {
+    this.view.init = this.init;
+    this.view.preShow = this.preShow;
+  },
 
- });
+  init: function() {
+    this.view.ContactsContainer.onClick = this.clicked;
+    this.view.PhoneFlexContainer.
+  },
+
+  preShow: function() {
+    var contact = kony.store.getItem("contactDetails");
+    this.view.ContactTextFlexContainer.NameLbl.text = contact.name;
+    this.view.ContactImgFlxContainer.ContactImg.src = contact.src;
+    this.view.PhoneFlexContainer.txtPhoneRounded.text = contact.phone;
+    this.view.EmailFlexContainer.EmailBox.text = contact.email;
+  },
+
+  clicked: function(){
+    var ntf = new kony.mvc.Navigation("ContactList");
+    ntf.navigate();
+  }
+
+});
